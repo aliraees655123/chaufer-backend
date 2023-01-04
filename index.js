59ec6daea6f8f889eee2941f0b1c473e6a08bcb9
aliraees655123 <96068596+aliraees655123@users.noreply.github.com>
2023-01-04 11:15:21 +0500

    ss

--- index.js
+++ index.js
@@ -63,17 +63,11 @@ mongoose
 
 
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT,()=>{
-//   console.log(`listening on port ${PORT}`);
-
-// })
-
-
-// const PORT = process.env.PORT || 5000;
-
-app.listen(5000,()=>{
-  console.log(`listening on port ${5000}`);
+app.listen(PORT,()=>{
+  console.log(`listening on port ${PORT}`);
 
 })
+
+
