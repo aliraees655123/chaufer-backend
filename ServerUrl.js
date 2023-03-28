fe1ef5f8113711e8547db87978de6a1f6c2ece24
aliraees655123 <96068596+aliraees655123@users.noreply.github.com>
2023-03-28 06:45:26 +0500

    ss

--- ServerUrl.js
+++ ServerUrl.js
@@ -1,4 +1,4 @@
 // export const SOCKETURL = `https://backend.thedresscode.studio/`;
-export const SERVERURL = `http://localhost:5000`;
+// export const SERVERURL = `http://localhost:5000`;
 // export const SERVERURL = `https://chaufer-backend-production.up.railway.app/api`;
-// export const SERVERURL = `https://happy-rose-hat.cyclic.app/api`;
+export const SERVERURL = `https://happy-rose-hat.cyclic.app`;
